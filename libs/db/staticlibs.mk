66b3ed6e5239ea02170455bf47870893b5bd8b5b
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2014-12-28 22:42:49 +0600

    We fixed a linking problem

--- libs/db/staticlibs.mk
+++ libs/db/staticlibs.mk
@@ -1,5 +1,5 @@
 
 DB_CSOURCES=$(wildcard $(SHOTODOL_DB_HOME)/libs/db/vsrc/*.c)
 DB_VSOURCE_BASE=$(basename $(notdir $(DB_CSOURCES)))
-OBJECTS+=$(addprefix $(SHOTODOL_DB_HOME)/build/.objects/, $(addsuffix .o,$(DB_VSOURCE_BASE)))
+OBJECTS+=$(addprefix $(SHOTODOL_DB_HOME)$(OBJDIR_COMMON)/, $(addsuffix .o,$(DB_VSOURCE_BASE)))
 
